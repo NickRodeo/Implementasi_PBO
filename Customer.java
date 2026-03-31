9a29c0579084b258ac459ec633b480d0c0782826
NickRodeo <rodeonick708@gmail.com>
2026-03-31 22:07:25 +0700

    Customer

--- Customer.java
+++ Customer.java
@@ -1,13 +1,24 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Customer {
     String name;
     String deliveryAddress;
     String contact;
     boolean active;
+    private List<Order> orders;
 
     public Customer(String name, String deliveryAddress, String contact, boolean active) {
         this.name = name;
         this.deliveryAddress = deliveryAddress;
         this.contact = contact;
         this.active = active;
+        //Bentuk relasi (Customer mempunyai 0 atau lebih OrderDetail)
+        this.orders = new ArrayList<>();
     }
+
+    public void addOrder(Order order) {
+        orders.add(order);
+    }
+
 }

d06374fc2cd5b13525aa03f407e6f3c192105b3f
NickRodeo <rodeonick708@gmail.com>
2026-03-31 22:04:10 +0700

    a

--- Item.java
+++ Item.java
@@ -1,11 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Item {
-    
     float weight;
     String description;
+    private List<OrderDetail> orderDetails;
 
     public Item(float weight, String description) {
         this.weight = weight;
         this.description = description;
+
+        //Bentuk relasi (item mempunyai 0 atau lebih OrderDetail)
+        this.orderDetails = new ArrayList<>();
     }
     
     public void getPriceForQuantity(){
@@ -14,6 +20,11 @@ public class Item {
 
     public void getWeight(){
 
+    }
+
+    //Method buat menambahkan OrderDetail
+    public void addOrderDetail(OrderDetail detail) {
+        orderDetails.add(detail);
     }
     
 }
--- OrderDetail.java
+++ OrderDetail.java
@@ -2,6 +2,18 @@ public class OrderDetail{
 
     int qty;
     String taxStatus;
+    Item item;
+    Order order;
+    
+    public OrderDetail(int qty, String taxStatus, Item item, Order order){
+        this.qty = qty;
+        this.taxStatus = taxStatus;
+        
+        //Bentuk relasi (OrderDetail mempunyai tepat 1 Item)
+        this.item = item; 
+        //Bentuk relasi (OrderDetail mempunyai tepat 1 Order)
+        this.order = order; 
+    }
 
     public void calculateSubTotal(){
 
